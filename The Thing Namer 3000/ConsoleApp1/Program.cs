04b38095c19e8d0aa9feba46fef954cfe47786a0
Kelvin  Joel Piña Sledzinski <75276742+KelvinJPS@users.noreply.github.com>
2024-01-23 20:59:39 -0500

    Add project files.

---
+++ The Thing Namer 3000/ConsoleApp1/Program.cs
@@ -0,0 +1,14 @@
+﻿// A program that finds a name for things
+
+/* Get the required info (Type and description) 
+ for the program to find an appropiate name */
+Console.WriteLine("What kind of thing are we talking about?"); 
+string type = Console.ReadLine(); 
+
+Console.WriteLine("How would you describe it? Big? Azure? Tattered?"); 
+string description = Console.ReadLine(); 
+
+// Name and current version of the program 
+string name = "Doom";
+string version = "3000"; 
+Console.WriteLine("The " + type + " " + description + " of " + c + " " + d + "!");
