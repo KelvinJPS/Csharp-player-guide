a12512630192c2713fea01943002644ceef6763c
Kelvin  Joel Piña Sledzinski <75276742+KelvinJPS@users.noreply.github.com>
2024-01-25 18:54:35 -0500

    triangle area calculator build

---
+++ The Triangle Farmer/The Triangle Farmer/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine("Input triangle size");
+int sizeTriangle = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Input triangle height");
+int heightInput = Convert.ToInt32(Console.ReadLine());
+
+int Area = (sizeTriangle * heightInput) / 2;
+Console.WriteLine(Area);
