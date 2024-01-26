8e791d2e039d427e079161d909bb4bc349ded766
Kelvin  Joel Piña Sledzinski <75276742+KelvinJPS@users.noreply.github.com>
2024-01-25 19:34:07 -0500

    solution completed

---
+++ The Four Sisters and the Duckbear/The Four Sisters and the Duckbear/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Eggs number");
+int chocolateEggs  = Convert.ToInt32(Console.ReadLine());
+int sistersCount = 4; 
+
+int sistersEggs =  chocolateEggs / sistersCount;
+int duckbeardEggs =  chocolateEggs % sistersCount;
+Console.WriteLine("Each sister gets {0} and the duckbear {1}", sistersEggs, duckbeardEggs);
+
+/*
+Answer this question: What are three total egg counts where the duckbear gets more than each 
+sister does? Yo u can use the program you created to help you find the answer. 
+When there is less than 4 eggs the sister get 0 eggs and the duckbear gets all of them.
+ */
