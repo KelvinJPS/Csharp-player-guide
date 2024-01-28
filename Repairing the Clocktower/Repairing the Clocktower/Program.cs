efe7975a8c23ffd82a49a26d71d838ddffbf8085
Kelvin  Joel Piña Sledzinski <75276742+KelvinJPS@users.noreply.github.com>
2024-01-27 21:05:57 -0500

    Repairing the Clocktower solution added

---
+++ Repairing the Clocktower/Repairing the Clocktower/Program.cs
@@ -0,0 +1,15 @@
+﻿/*
+ * Objectives: • Take a number as input from the console. 
+ * Display the word “Tick” if the number is even. 
+ * Display the word “Tock” if the number is odd. 
+ */
+Console.WriteLine("insert number ");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number % 2 == 0)
+{
+    Console.WriteLine("Tick");
+}
+else
+{
+    Console.WriteLine("Tock");
+}
---
+++ mathtest/mathtest/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+int a = 4; 
+int b = 5;
+var  result =  (float) b / a;
+Console.WriteLine(result);
+
+/// 
+int health = 0;
+health += 120; 
+health = Math.Clamp(health, 0, 100); // Keep it in the interval 0 to 100.
+Console.WriteLine(health);
+// with clamp it doesn't go up that value 
