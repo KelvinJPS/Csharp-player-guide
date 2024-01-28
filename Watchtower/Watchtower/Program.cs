3ac06302467d2f0ebc3c952f0ac61cefdaa1ab17
Kelvin  Joel Piña Sledzinski <75276742+KelvinJPS@users.noreply.github.com>
2024-01-27 22:13:18 -0500

    checking value coordinates of x and y

---
+++ Watchtower/Watchtower/Program.cs
@@ -0,0 +1,61 @@
+﻿
+// Get x and y from user
+Console.WriteLine("Provide value of x:");
+int x = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Provide value of y:");
+int y = Convert.ToInt32(Console.ReadLine());
+
+if (y < 0)
+{
+    if (x < 0)
+    {
+        Console.WriteLine("The enemy is to the southwest");
+    }
+    else if (x == 0)
+    {
+        Console.WriteLine("The enemy is to the south");
+    }
+    else if (x > 0)
+    {
+        Console.WriteLine("The enemy is to the southeast");
+
+    }
+else if (y == 0)
+    {
+    if (x < 0)
+    {
+        Console.WriteLine("The enemy is to the west");
+    }
+    else if (x == 0)
+    {
+        Console.WriteLine("The enemy is here");
+    }
+    else if (x > 0)
+    {
+        Console.WriteLine("The enemy is to the east");
+
+    }
+    }
+
+else if (y > 0)
+    {
+        if (x < 0)
+        {
+            Console.WriteLine("The enemy is to the orthwest");
+        }
+        else if (x == 0)
+        {
+            Console.WriteLine("The enemy is to north");
+        }
+        else if (x > 0)
+        {
+            Console.WriteLine("The enemy is to the northeast");
+
+        }
+    }
+}   
+
+
+
+    
+
