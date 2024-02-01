f9e890fe4ab60df0f2f0440a907b8c0f8946a748
Kelvin  Joel Piña Sledzinski <75276742+KelvinJPS@users.noreply.github.com>
2024-01-31 22:14:02 -0500

    The prototype solution

---
+++ The_Prototype/The_Prototype/Program.cs
@@ -0,0 +1,35 @@
+﻿int pilotNumber;
+int hunterNumber;
+
+do
+{
+    
+    Console.WriteLine("pick a number between 0 - 100");
+    pilotNumber = Convert.ToInt32(Console.ReadLine());
+}
+while (pilotNumber < 0 || pilotNumber > 100);
+
+do
+{
+
+    Console.WriteLine("Guess the pilot's number");
+    hunterNumber = Convert.ToInt32(Console.ReadLine());
+
+    if (hunterNumber > pilotNumber)
+    {
+        Console.WriteLine("Too high");
+    }
+
+    else if (hunterNumber < pilotNumber)
+    {
+        Console.WriteLine("Too low");
+    }
+    
+    else
+    {
+        Console.WriteLine("You guessed the number");
+    }
+
+}
+while (hunterNumber != pilotNumber);
+
