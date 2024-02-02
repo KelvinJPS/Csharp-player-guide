2a1f14d1f0ec1561507bcd9268245eebad6cde9e
Kelvin  Joel Piña Sledzinski <75276742+KelvinJPS@users.noreply.github.com>
2024-02-02 06:51:09 -0500

    The magic Cannon full solution tested

---
+++ The_Magic_Cannon/The_Magic_Cannon/Program.cs
@@ -0,0 +1,28 @@
+﻿
+for (int i = 1, fireI = 1, electricI = 1; i < 100; i++, fireI++, electricI++)
+{
+    if (fireI == 3)
+    {
+        Console.WriteLine("Fire");
+        fireI = 0;
+    }
+
+    else if (electricI == 5)
+    {
+        Console.WriteLine("Electric");
+        electricI = 0;
+    }
+    else
+    {
+        Console.WriteLine("Normal");
+    }
+    // Reset fireI and electricI After every 10 iterations
+
+    if (i % 10 == 0)
+    {
+        fireI = 1;
+        electricI = 1;
+    }
+}
+
+
