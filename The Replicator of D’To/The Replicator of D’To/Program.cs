f7f754bd6fd757a67b66c73eb9d9312973c1e2a8
Kelvin  Joel Piña Sledzinski <75276742+KelvinJPS@users.noreply.github.com>
2024-02-04 19:50:48 -0500

    The Replicator of D’To

---
+++ The Replicator of D’To/The Replicator of D’To/Program.cs
@@ -0,0 +1,22 @@
+﻿
+int[] userNumbers = new int[5];
+
+Console.WriteLine("input numbers");
+
+for(int i = 0; i < 5; i++)
+{
+    Console.WriteLine("input number");
+    int n = Convert.ToInt32(Console.ReadLine());
+    userNumbers[i] = n;
+
+}
+// copy values 
+int [] userNumbersCopy = new int[userNumbers.Length];
+
+for (int i = 0; i < userNumbers.Length; i++)
+{
+    userNumbersCopy[i] = userNumbers[i];
+}
+
+for (int index = 0; index < 5; index++)
+    Console.WriteLine($"{userNumbers[index]} and {userNumbersCopy[index]}");
