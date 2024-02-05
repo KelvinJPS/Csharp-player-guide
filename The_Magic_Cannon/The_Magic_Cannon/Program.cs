3791592af9fdfe567df22cc14dec74c71c1a4a4c
Kelvin  Joel Piña Sledzinski <75276742+KelvinJPS@users.noreply.github.com>
2024-02-04 19:07:33 -0500

    fixed electric conditional and added comments

--- The_Magic_Cannon/The_Magic_Cannon/Program.cs
+++ The_Magic_Cannon/The_Magic_Cannon/Program.cs
@@ -2,20 +2,24 @@
 
 for (int i = 1;  i < 100; i++)
 {
-
+     // Electric and fire are at they desired value
     if( i % 3  == 0 && i % 5 == 0 )
     {
         Console.ForegroundColor = ConsoleColor.Cyan;
         Console.WriteLine("Electric and fire");
     }
     
+    // each multiple of 3 
    else if (i % 3 == 0)
     {
         Console.ForegroundColor = ConsoleColor.Red;
         Console.WriteLine("Fire");
     }
 
-    else if (i == 5)
+
+    // each multiple of 5
+
+    else if (i % 5 == 0)
     {
         Console.ForegroundColor = ConsoleColor.Yellow;
         Console.WriteLine("Electric");
