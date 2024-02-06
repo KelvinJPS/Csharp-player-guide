b3a7ab61f6d2c6c2f9a883f0586ffc918022a711
Kelvin  Joel Piña Sledzinski <75276742+KelvinJPS@users.noreply.github.com>
2024-02-06 07:41:28 -0500

    the_laws_of_freach

---
+++ the_laws_of_freach/the_laws_of_freach/Program.cs
@@ -0,0 +1,9 @@
+﻿int[] array = new int[] { 4, 51, -7, 13, -99, 15, -8, 45, 90 };
+int total = 0;
+
+for (int index = 0; index < array.Length; index++)
+foreach (int inder in array)
+        total += array[index];
+
+float average = (float)total / array.Length;
+Console.WriteLine(average);
