d62ef558b345990b9e21008e449126ee556a7ea5
Kelvin  Joel Piña Sledzinski <75276742+KelvinJPS@users.noreply.github.com>
2024-02-06 08:06:19 -0500

    taking a number

---
+++ Taking a Number/Taking a Number/Program.cs
@@ -0,0 +1,20 @@
+﻿int askForNumber (string text)
+{
+    Console.WriteLine (text);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int askForNumberInRange(string text, int min, int max)
+{
+    int number;
+    do
+    {
+        Console.WriteLine(text);
+        number = Convert.ToInt32(Console.ReadLine());
+    }
+    while (number < min || number > max);
+    return number;
+}
+
+int result = askForNumber("What is the airspeed velocity of an unladen swallow?");
+askForNumberInRange("Provide a number between 1-10", 1, 10);
